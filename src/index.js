1693af5f67624b3dff50f19e0177a9086819ffd3
yujo <yujo.dev@gmail.com>
2021-11-15 20:23:36 +0900

    feat: todoItem을 추가하는 기능

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('hellos');
+import { $ } from './utils/querySelector';
+
+const $todoList = $('.todo-list');
+const $newTodo = $('.new-todo');
+
+const todoItemTemplate = ({ id, text }) => `<li id=${id}>
+                                              <div class="view">
+                                                <input class="toggle" type="checkbox" />
+                                                <label class="label">${text}</label>
+                                                <button class="destroy"></button>
+                                              </div>
+                                              <input class="edit" value="${text}" />
+                                            </li>`;
+
+const addTodoItem = ({ key, target }) => {
+  if (key !== 'Enter') return;
+
+  if (target.value.trim().length < 1) return;
+
+  $todoList.insertAdjacentHTML(
+    'beforeend',
+    todoItemTemplate({ id: Date.now(), text: target.value })
+  );
+
+  target.value = '';
+};
+
+const app = () => {
+  $newTodo.addEventListener('keyup', addTodoItem);
+};
+
+window.onload = () => {
+  app();
+};
