ac85ea6bfc6324b4a54c0a9686e5b65a82deda33
yoonhe <yhehe228@gmail.com>
2021-11-16 19:15:24 +0900

    Feat : Todo Item 추가 기능 구현

---
+++ index.js
@@ -0,0 +1,31 @@
+const $ = (selector) => document.querySelector(selector);
+
+const $form = $("#todo-form");
+const $input = $("#new-todo-title");
+const $todoList = $("#todo-list");
+
+const createTemplate = ({ value }) => `
+<li>
+  <div class="view">
+    <input class="toggle" type="checkbox" />
+    <label class="label">${value}</label>
+    <button class="destroy"></button>
+  </div>
+  <input class="edit" value="${value}" />
+</li>
+`;
+
+const clearInput = () => {
+  $input.value = "";
+};
+
+$form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  $todoList.insertAdjacentHTML(
+    "beforeend",
+    createTemplate({ value: $input.value })
+  );
+
+  clearInput();
+});
