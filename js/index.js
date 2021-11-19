abcce6f891d2a85806903996f2ace90abe22d39b
jinsoobyeon <jinsoobyeon@gmail.com>
2021-11-19 17:54:39 +0900

    Todo List에 Item을 키보드로 입력하여 추가

--- js/index.js
+++ js/index.js
@@ -1,24 +1,48 @@
 // STEP 01 요구사항 분석
 // Todo List 추가
-// - [ ] Todo List에 Item을 키보드로 입력하여 추가하기
-// - [ ] 추가되는 item의 마크업은 `<ul id="todo-list" class="todo-list"></ul>` 안에 삽입해야 한다.
-// - [ ] 총 item 갯수를 count하여 하단에 보여준다.
-// - [ ] item이 추가되고 나면, input은 빈 값으로 초기화한다.
-// - [ ] 사용자 입력값이 빈 값이라면 추가되지 않는다.
+// - [x] item의 title을 입력 받고 엔터키 입력으로 추가한다.
+// - [x] 추가되는 item의 마크업은 `<ul id="todo-list" class="todo-list"></ul>` 안에 삽입해야 한다.
+// - [x] 총 item 갯수를 count하여 하단에 보여준다.
+// - [x] item이 추가되고 나면, input은 빈 값으로 초기화한다.
+// - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 
 // Todo List 삭제
 // - [ ] Todo List의 x버튼을 이용해서 해당 엘리먼트를 삭제하기
 // - [ ] 총 item 갯수를 count하여 하단에 보여준다.
 
+const $ = (selector) => document.querySelector(selector)
+
 function App() {
   // enter를 누르면 item의 이름을 입력받는다.
-  document
-    .querySelector('#new-todo-title')
-    .addEventListener('keypress', (e) => {
-      if (e.key === 'Enter') {
-        console.log(document.querySelector('#new-todo-title').value)
+  $('#new-todo-title').addEventListener('keypress', (e) => {
+    if ($('#new-todo-title').value === '') {
+      return
+    }
+
+    if (e.key === 'Enter') {
+      const newTodoTitle = $('#new-todo-title').value
+      const todoTemplate = (newTodoTitle) => {
+        return `<li>
+          <div class="view">
+            <input class="toggle" type="checkbox" />
+            <label class="label">${newTodoTitle}</label>
+            <button class="destroy"></button>
+          </div>
+          <input class="edit" value="${newTodoTitle}" />
+        </li>`
       }
-    })
+
+      $('#todo-list').insertAdjacentHTML(
+        'beforeend',
+        todoTemplate(newTodoTitle)
+      )
+
+      const todoCount = $('#todo-list').querySelectorAll('li').length
+      $('.todo-count strong').innerText = todoCount
+
+      $('#new-todo-title').value = ''
+    }
+  })
 }
 
 App()
