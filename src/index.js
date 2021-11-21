a0ca4c40a18dea2ee3c6d0ec8503c161e7ce0a44
Chung Chanhoo <jchan1001@naver.com>
2021-11-21 20:10:29 +0900

    리펙토링, 코드 리뷰 반영
    리펙토링
      1. script 읽어오는 방식을 defer로 변경
      2. 함수명을 조금 더 알기 쉽게 변경
    
    코드 리뷰 반영
      1. 파싱한 DOM 요소들을 블록 레벨로 이동
        https://github.com/remote-develop-study/js-clean-todo/pull/3#discussion_r752955374
      2. 템플릿 생성용 함수 추가
        https://github.com/remote-develop-study/js-clean-todo/pull/3#discussion_r752957467
      3. 컨벤션 통일
        https://github.com/remote-develop-study/js-clean-todo/pull/3#discussion_r752958971

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 'use strict'
 
 const $ = (selector) => document.querySelector(selector);
-const $todoList = $('#todo-list');
-const $newTitle = $('#new-todo-title');
 
 /**
  * {@link key}가 입력하는 키인지 확인
@@ -15,13 +13,13 @@ const $newTitle = $('#new-todo-title');
 }
 
 /**
- * TodoItem 추가
+ * {@link todoList}에 TodoItem 추가
+ * @param  {Object} todoList - 추가할 리스트
  * @param  {String} title - 추가할 내용
  */
-const addTodoItem = (title) => {
-  if (validateTodoTitle(title)) {
-    insertTodoItem(title);
-    clearTodoInput();
+const addTodoItem = (todoList, title) => {
+  if (isValidateTodoTitle(title)) {
+    insertTodoItem(todoList, title);
   }
 }
 
@@ -38,17 +36,26 @@ const addTodoItem = (title) => {
  * @param  {String} title - 제목
  * @return {Boolean}
  */
-const validateTodoTitle = (title) => {
+const isValidateTodoTitle = (title) => {
   return title.replace(/\s/g, '').length > 0;
 }
 
 /**
  * TodoItem 표시
+ * @param  {Object} todoList - 표시할 리스트
  * @param  {String} title - 제목
  */
-const insertTodoItem = (title) => {
-  $todoList.insertAdjacentHTML('afterbegin',
-  `
+const insertTodoItem = (todoList, title) => {
+  todoList.insertAdjacentHTML('afterbegin', createTodoItemHTML(title));
+}
+
+/**
+ * TodoItem용 HTML 생성
+ * @param  {String} title - 제목
+ * @return {String} HTML
+ */
+const createTodoItemHTML = (title) => {
+  return `
   <li>
     <div class="view">
       <input class="toggle" type="checkbox" />
@@ -57,24 +64,30 @@ const insertTodoItem = (title) => {
     </div>
     <input class="edit" value="${title}" />
   </li>
-  `);
+  `;
 }
 
 /**
- * Todo 제목 입력란 초기화
+ * 입력란 초기화
+ * @param  {Object} input - 초기화할 입력란
  */
-const clearTodoInput = () => {
-  $newTitle.value = '';
+const clearTodoInput = (input) => {
+  input.value = '';
 }
 
 window.onload = () => {
-  $newTitle.addEventListener('keyup', (event) => {
+  const $todoList = $('#todo-list');
+
+  $('#new-todo-title').addEventListener('keyup', function(event) {
     if (isAddTodoItemKey(event.key)) {
-      addTodoItem(event.target.value);
+      addTodoItem($todoList, event.target.value);
+      clearTodoInput(this);
     }
   });
 
   $todoList.addEventListener('click', (event) => {
-    if (event.target.name === 'destroyBtn') removeParentTodoItem(event.target);
+    if (event.target.name === 'destroyBtn') {
+      removeParentTodoItem(event.target);
+    }
   });
 };
