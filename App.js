3ec86ee3e77513049234a6895f28425359837c83
yoonhe <yhehe228@gmail.com>
2021-11-24 09:12:08 +0900

    Refactor : TodoList 컴포넌트가 상태를 기준으로 렌더링되도록 변경

--- App.js
+++ App.js
@@ -4,12 +4,32 @@ import TodoList from "./TodoList.js";
 
 export default class App {
   constructor({ $app }) {
+    this.state = {
+      mode: "view", // "view" | "editing" | "completed"
+      list: [
+        {
+          id: 1,
+          name: "과제하기",
+          checked: true,
+        },
+        {
+          id: 2,
+          name: "과제하기",
+          checked: false,
+        },
+      ],
+    };
+
     $app.innerHTML = `
       <h1>TODOS</h1>
     `;
 
-    this.$form = new TodoForm({ $app });
-    this.$todoList = new TodoList({ $app });
-    this.$todoCountContainer = new TodoCountContainer({ $app });
+    new TodoForm({ $app });
+    this.todoList = new TodoList({
+      $app,
+      mode: this.mode,
+      list: this.state.list,
+    });
+    new TodoCountContainer({ $app });
   }
 }
--- TodoList.js
+++ TodoList.js
@@ -1,13 +1,48 @@
 import { createElement } from "./utils/domCreator.js";
 
 export default class TodoList {
-  constructor({ $app }) {
+  constructor({ $app, mode, list }) {
+    this.state = {
+      mode,
+      list,
+    };
+
     this.$list = createElement("ul");
     this.$list.className = "todo-list";
-    this.$list.innerHTML = `
-      <ul id="todo-list" class="todo-list"></ul>
-    `;
 
     $app.appendChild(this.$list);
+
+    this.render();
+  }
+
+  setState(nextState) {
+    this.state = nextState;
+
+    this.render();
+  }
+
+  render() {
+    this.$list.innerHTML = this.template();
+  }
+
+  template() {
+    return `
+      <ul>
+        ${this.state.list
+          .map(
+            ({ id, name, checked }) => `
+        <li id="${id}" class="${this.state.mode}">
+          <div>
+            <input class="toggle" type="checkbox" ${checked ? "checked" : ""} />
+            <label class="label">${name}</label>
+            <button class="destroy"></button>
+          </div>
+          <input class="edit" value="${name}" />
+        </li>
+      `
+          )
+          .join("")}
+      </ul>
+    `;
   }
 }
